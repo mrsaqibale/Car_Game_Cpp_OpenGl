13d6434ccc49cdc348eeb50db3e05e493d867546
codewithprofessor <kalikali72547254@gmail.com>
2024-05-25 09:21:42 +0500

    update

--- test.cpp
+++ test.cpp
@@ -15,30 +15,25 @@ const float ROAD_WIDTH = 0.6f;
 const float LANE_WIDTH = ROAD_WIDTH / 3;
 float roadOffset = 0.0f;  // Used to animate the moving road
 
-void drawRoad() {
+void drawSideOfRoad() {
     glBegin(GL_QUADS);
-    // Draw the road
-    glColor3f(0.2f, 0.2f, 0.2f);
+    glColor3f(0.0f, 1.0f, 0.0f); // Green color
+    // Left side
+    glVertex2f(-1.0f, -1.0f);
     glVertex2f(-ROAD_WIDTH / 2, -1.0f);
-    glVertex2f(ROAD_WIDTH / 2, -1.0f);
-    glVertex2f(ROAD_WIDTH / 2, 1.0f);
     glVertex2f(-ROAD_WIDTH / 2, 1.0f);
-
-    // Draw lane dividers
-    glColor3f(1.0f, 1.0f, 1.0f);
-    for (float i = -1.0f + roadOffset; i < 1.0f; i += 0.2f) {
-        glVertex2f(-LANE_WIDTH / 2, i);
-        glVertex2f(-LANE_WIDTH / 2, i + 0.1f);
-        glVertex2f(LANE_WIDTH / 2, i + 0.1f);
-        glVertex2f(LANE_WIDTH / 2, i);
-    }
+    glVertex2f(-1.0f, 1.0f);
+    // Right side
+    glVertex2f(ROAD_WIDTH / 2, -1.0f);
+    glVertex2f(1.0f, -1.0f);
+    glVertex2f(1.0f, 1.0f);
+    glVertex2f(ROAD_WIDTH / 2, 1.0f);
     glEnd();
 }
 
 void drawCar() {
     glBegin(GL_QUADS);
-    // Draw the car
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f); // Red color
     glVertex2f(carPosX - CAR_WIDTH / 2, CAR_POS_Y - CAR_HEIGHT / 2);
     glVertex2f(carPosX + CAR_WIDTH / 2, CAR_POS_Y - CAR_HEIGHT / 2);
     glVertex2f(carPosX + CAR_WIDTH / 2, CAR_POS_Y + CAR_HEIGHT / 2);
@@ -46,25 +41,53 @@ void drawCar() {
     glEnd();
 }
 
+void drawWhiteLines() {
+    glColor3f(1.0f, 1.0f, 1.0f); // White color
+    glBegin(GL_LINES);
+    // Left lane line
+    glVertex2f(-LANE_WIDTH / 2, -1.0f);
+    glVertex2f(-LANE_WIDTH / 2, 1.0f);
+    // Right lane line
+    glVertex2f(LANE_WIDTH / 2, -1.0f);
+    glVertex2f(LANE_WIDTH / 2, 1.0f);
+    glEnd();
+}
+
+void drawYellowLines() {
+    glColor3f(1.0f, 1.0f, 0.0f); // Yellow color
+    glBegin(GL_LINES);
+    for (float i = -1.0f + roadOffset; i < 1.0f; i += 0.2f) {
+        glVertex2f(-ROAD_WIDTH / 2, i);
+        glVertex2f(ROAD_WIDTH / 2, i);
+    }
+    glEnd();
+}
+
+void drawRoad() {
+    glBegin(GL_QUADS);
+    glColor3f(0.2f, 0.2f, 0.2f); // Dark grey color for road
+    glVertex2f(-ROAD_WIDTH / 2, -1.0f);
+    glVertex2f(ROAD_WIDTH / 2, -1.0f);
+    glVertex2f(ROAD_WIDTH / 2, 1.0f);
+    glVertex2f(-ROAD_WIDTH / 2, 1.0f);
+    glEnd();
+}
+
+void moveRoad() {
+    roadOffset -= 0.02f;
+    if (roadOffset < -0.2f) {
+        roadOffset = 0.0f;
+    }
+}
+
 void renderScene() {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
 
-    // Draw green sides
-    glBegin(GL_QUADS);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2f(-1.0f, -1.0f);
-    glVertex2f(-ROAD_WIDTH / 2, -1.0f);
-    glVertex2f(-ROAD_WIDTH / 2, 1.0f);
-    glVertex2f(-1.0f, 1.0f);
-
-    glVertex2f(ROAD_WIDTH / 2, -1.0f);
-    glVertex2f(1.0f, -1.0f);
-    glVertex2f(1.0f, 1.0f);
-    glVertex2f(ROAD_WIDTH / 2, 1.0f);
-    glEnd();
-
+    drawSideOfRoad();
     drawRoad();
+    drawWhiteLines();
+    drawYellowLines();
     drawCar();
 
     glutSwapBuffers();
@@ -78,10 +101,7 @@ void initDisplay() {
 }
 
 void timer(int) {
-    roadOffset -= 0.02f;
-    if (roadOffset < -0.2f) {
-        roadOffset = 0.0f;
-    }
+    moveRoad();
     glutPostRedisplay();  // Post a paint request to activate display()
     glutTimerFunc(16, timer, 0);  // ~60 FPS
 }
